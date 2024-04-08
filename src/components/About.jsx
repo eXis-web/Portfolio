24900c9dd9cbba281b90414741a39a0fe661642e
Ksenia <varosianarmen92@gmail.com>
2024-04-08 22:54:57 +0200

    Update About component with additional content
    This commit updates the About component by adding a new introduction paragraph and modifying the existing paragraph to improve readability. The title of the About section has also been included for better context.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Component for rendering the About section
 const About = () => {
   return (
     <div
@@ -8,20 +9,23 @@ const About = () => {
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
+          {/* Title of the About section */}
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
 
+        {/* Introduction paragraph */}
         <p className="text-xl mt-5">
           I am a frontend web developer with a passion for crafting engaging user experiences. Eager to enhance my skills and explore new technologies, I am actively seeking permanent employment opportunities while also taking on freelance projects.
         </p>
 
         <br />
 
+        {/* Additional details paragraph */}
         <p className="text-xl">
-        I thrive on building projects from the ground up and find joy in bringing ideas to fruition. Reach out to me to discuss how I can assist with your project. Whether you're looking for ongoing support or need a specific task completed, I'm here to help. <br /> <br />
-        I prioritize innovative solutions aimed at ensuring your project's success, focusing on creating unique experiences and enhancing conversion rates. Let's work together to find the best ways to achieve your goals!<br /> <br />
+          I thrive on building projects from the ground up and find joy in bringing ideas to fruition. Reach out to me to discuss how I can assist with your project. Whether you're looking for ongoing support or need a specific task completed, I'm here to help. <br /> <br />
+          I prioritize innovative solutions aimed at ensuring your project's success, focusing on creating unique experiences and enhancing conversion rates. Let's work together to find the best ways to achieve your goals!<br /> <br />
         </p>
       </div>
     </div>
