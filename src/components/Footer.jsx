699183a2fa887d2c485552f9416006040b7e09e6
Ksenia <varosianarmen92@gmail.com>
2024-05-18 22:20:43 +0200

    "Update link in Footer component to point to new URL"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="text-center p-6 bg-black footer">
       <span className="text-white">&copy; 2023 Copyright - </span>
-      <a className="link-underline text-white font-bold" href="https://rahulkarda.netlify.app">Armen Varosian</a>
+      <a className="link-underline text-white font-bold" href="https://armen-varosian-portfolio.netlify.app/">Armen Varosian</a>
     </div>
   );
 }
